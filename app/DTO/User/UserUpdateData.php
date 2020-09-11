5a888ccc690af381f38711cb83b5e5ef8a45a7da
oleg90990 <oleg909902@yandex.ru>
2020-09-11 23:56:34 +0300

    fx

--- app/DTO/User/UserUpdateData.php
+++ app/DTO/User/UserUpdateData.php
@@ -17,7 +17,7 @@ class UserUpdateData extends DataTransferObject
         return new self([
             'name' => $request->get('name'),
             'email' => $request->get('email'),
-            'password' => $request->has('password') ? bcrypt($request->get('password')) : null,
+            'password' => $request->get('password') ? bcrypt($request->get('password')) : null,
             'city_id' => $request->get('city_id')
         ]);
     }
